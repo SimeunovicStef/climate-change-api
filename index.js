63497155189c93905846e7c81ec92fc9fdf12e62
SimeunovicStef <eskimofrogster@gmail.com>
2022-04-22 16:39:24 +0200

    final code for deployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -45,13 +45,38 @@ newspapers.forEach(newspaper => {
         })
 })
 
-app.get('/', (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>) => {
+app.get('/', (req, res) => {
     res.json(`Welcome to my Climate Change News API`)
 })
 
 app.get(`/news`, (req, res) => {
     res.json(articles)
 
+})
+
+app.get('/news/:newspaperId,', (req, res) => {
+ const newspaperId = req.params.newspaperId
+ 
+ const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+ const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+ axios.get(newspaperAddress)
+ .then(response => {
+     const html = response.data
+     const $ = cheerio.load(html)
+     const specificArticles = []
+
+     $('a:contains("climate")', html).each(function () {
+         const title = $(this).text()
+         const url = $(this).attr('href')
+         specificArticles.push({
+             title,
+             url: newspaperBase + url,
+             source: newspaperId
+         })
+     })
+     res.json(specificArticles)
+ }).catch(err => console.log(err))
 })
 
 app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
